d8e722bf3cc84e9dfbbf60956e41bd140c9bfb37
adammmertel <mertel.adam@gmail.com>
2019-05-14 19:20:59 +0200

    :sparkles:FEAT: Checkbox component

--- app/components/panel.tsx
+++ app/components/panel.tsx
@@ -9,10 +9,49 @@ export default class PanelComponent extends React.Component<Props> {
     super(props);
   }
 
+  toggleCheckbox(checkboxId) {
+    console.log(checkboxId);
+  }
+
+  renderCheckbox(data: { key; value; label; checked; event; style? }) {
+    return (
+      <div className="md:flex md:items-center mb-6" key={data.key}>
+        <label className="md:w-full block text-gray-500 font-bold">
+          <input
+            onChange={data.event.bind(this)}
+            checked={data.checked}
+            value={data.value}
+            id={data.key}
+            className="mr-2 mt-4 leading-tight"
+            type="checkbox"
+          />
+          <span className="text-sm align-text-top tb-2  font-normal">
+            {data.label}
+          </span>
+        </label>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="panel" data-testid="panel-wrapper">
-        <h1 className="heading text-muni">Map</h1>
+        <h1 className="heading text-muni">Lollards, 1414-1522</h1>
+        <h2 className="subheading text-black">
+          Places of origin of Lollards, unauthorized book owners, and
+          participants in revolts related to Lollardy
+        </h2>
+
+        <br />
+        <b>revolts</b>
+
+        {this.renderCheckbox({
+          key: 1,
+          value: "1414",
+          label: "1414 revolt",
+          checked: true,
+          event: this.toggleCheckbox
+        })}
 
         <button className="primary">primary button</button>
         <button className="secondary">secondary button</button>
