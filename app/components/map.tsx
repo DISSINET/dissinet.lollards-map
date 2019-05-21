6e584fe5ef0ba9bd27f8d542dcb0b94793eae460
adammmertel <mertel.adam@gmail.com>
2019-05-22 01:00:00 +0200

    :hammer:REFACTOR: Map symbol

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -46,7 +46,7 @@ export default class MapComponent extends React.Component<Props> {
 
       this.clusters = L.markerClusterGroup({
         showCoverageOnHover: false,
-        maxClusterRadius: 70,
+        maxClusterRadius: 50,
         firstCircleElements: 6,
         clockHelpingCircleOptions: {
           weight: 0.7,
@@ -103,12 +103,37 @@ export default class MapComponent extends React.Component<Props> {
     const svgEl = document.createElement("svg");
     svgEl.setAttribute("id", "marker " + cluster._leaflet_id);
 
-    const dFire1 =
-      "m 2.9990256,270.82817 c 1.0690017,1.51304 0.2844093,2.84512 -1.1792074,5.0188 -1.46721995,2.17903 -3.1687443,7.25758 2.3157712,7.1604 7.1179906,-0.12612 6.6972696,-7.23466 -1.1365638,-12.1792 z";
-    const dFire2 =
-      "m 23.515284,270.81949 c -1.086021,1.51304 -0.288938,2.84512 1.19798,5.0188 1.490579,2.17903 3.219193,7.25758 -2.352639,7.1604 -7.231312,-0.12612 -6.803893,-7.23466 1.154659,-12.1792 z";
-    const dBook =
-      "m 26.058778,286.50967 -6.715147,2.10475 -5.34539,-1.16931 -0.133635,8.08491 5.846521,1.06907 6.381059,-1.9377 z";
+    const dFire =
+      "m 10.859695,296.52825 c 0,0 -8.9449934,-0.43314 -9.0537544,-6.94902 -0.1086139,-6.50707 7.3176902,-11.6259 9.2542064,-18.57522 1.569022,10.55683 12.932719,11.69244 12.695304,18.90932 -0.101322,5.67974 -10.156242,6.64833 -10.156242,6.64833 0,0 6.013563,-1.43657 5.679477,-4.87767 0.03341,-2.93997 -2.639287,-3.67496 -4.109269,-6.14721 -0.777706,2.01011 -2.037931,3.14042 -2.80633,3.17383 -0.7684,0.0334 -1.610499,-2.60816 -2.8731476,-5.67947 -0.627883,4.11156 -3.2190346,5.71501 -3.3408692,8.15171 -0.2004518,4.00905 4.7106248,5.3454 4.7106248,5.3454 z";
+    const dBook = `
+    <g
+      style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.67237687;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+      transform="translate(0,-160) scale(0.6 0.6)">
+     <path
+        style="fill:none;stroke:#000000"
+        d="m 24.114374,290.37551 -16.2169117,0.006 0.08977,3.05488 15.9959607,-0.0873"
+        id="path870-2" />
+     <path
+        style="fill:#000000;stroke:#000000"
+        d="m 7.7984524,293.43039 -5.3810214,-15.49195 -0.053384,-2.94076 5.3445284,15.37783 0.08977,3.05488"
+        />
+     <path
+        style="fill:#000000;stroke:#000000"
+        d="m 2.3640894,274.99768 15.8123126,0.01 6.109986,15.36475 H 7.7086074 Z"
+         />
+     <path
+        style="fill:#ffffff;stroke:none"
+        d="m 19.333204,276.87518 -6.289154,0.0242 0.942515,2.12141 6.266267,-0.011 z"
+         />
+     <path
+        style="fill:#ffffff;stroke:none"
+        d="m 22.662633,285.30666 -6.461403,0.0421 0.959766,2.10418 6.421339,-0.011 z"
+    />
+     <path
+        style="fill:none;stroke:#ffffff"
+        d="m 3.9152936,276.58523 4.2905132,12.23799"
+  />
+   </g>`;
 
     const svg = d3
       .select(svgEl)
@@ -122,7 +147,10 @@ export default class MapComponent extends React.Component<Props> {
       .append("g")
       .style("stroke", "black")
       .attr("class", "cluster-symbols")
-      .attr("transform", "translate(5 -540) scale(2 2)");
+      .attr(
+        "transform",
+        "translate(" + svgSizeX / 2 + " " + svgSizeY / 2 + ") "
+      );
 
     const gText = svg
       .append("g")
@@ -131,60 +159,90 @@ export default class MapComponent extends React.Component<Props> {
       .style("text-anchor", "middle")
       .attr("transform", "translate(25 35)");
 
-    gSymbols
-      .append("circle")
-      .attr("class", "total")
-      .attr("cx", "10")
-      .attr("cy", "287")
-      .attr("r", 8);
+    if (markers.length > 1) {
+      const r = 16;
+      gSymbols
+        .append("circle")
+        .attr("class", "total")
+        .style("stroke", "white")
+        .style("stroke-width", "2")
+        .attr("x", svgSizeX / 2)
+        .attr("y", svgSizeY / 2)
+        .attr("r", r);
+      /*
+        gText
+        .append("text")
+        .text(markers.length)
+        .attr("class", "total")
+        .attr("font-size", "17")
+        .attr("transform", "translate(-1 5)");
+        */
+    } else {
+      const r = 10;
+      gSymbols
+        .append("circle")
+        .style("stroke", "white")
+        .style("stroke-width", "2")
+        .attr("class", "total")
+        .attr("x", svgSizeX / 2)
+        .attr("y", svgSizeY / 2)
+        .attr("r", r);
+    }
 
-    gText
-      .append("text")
-      .text(markers.length)
-      .attr("class", "total")
-      .attr("font-size", "17")
-      .attr("transform", "translate(-1 5)");
-
+    const revoltY = -12;
     if (revolt1No) {
+      gSymbols
+        .append("circle")
+        .attr("class", "total")
+        .attr("x", svgSizeX / 2)
+        .style("fill", "white")
+        .attr("y", svgSizeY / 2)
+        .attr("transform", "translate(-8 " + revoltY + ")")
+        .attr("r", 8);
       gSymbols
         .append("path")
         .attr("class", "revolt1")
-        .attr("d", dFire1);
-
-      gText
-        .append("text")
-        .text(revolt1No)
-        .attr("class", "revolt1")
-        .attr("transform", "translate(-10 -13)");
+        .attr("d", dFire)
+        .attr(
+          "transform",
+          "scale(0.5 0.5) translate(-28 " + (revoltY - 296) + ")"
+        );
     }
 
     if (revolt2No) {
       gSymbols
+        .append("circle")
+        .attr("class", "total")
+        .attr("x", svgSizeX / 2)
+        .style("fill", "white")
+        .attr("y", svgSizeY / 2)
+        .attr("transform", "translate(8 " + revoltY + ")")
+        .attr("r", 8);
+      gSymbols
         .append("path")
-        .attr("d", dFire2)
+        .attr("d", dFire)
         .attr("class", "revolt2")
-        .attr("transform", "translate(-7 0)");
-
-      gText
-        .append("text")
-        .text(revolt2No)
-        .attr("class", "revolt2")
-        .attr("transform", "translate(10 -13)");
+        .attr(
+          "transform",
+          "scale(0.5 0.5) translate(5 " + (revoltY - 296) + ")"
+        );
     }
 
     if (booksNo) {
       gSymbols
-        .append("path")
-        .attr("d", dBook)
-        .attr("class", "book")
-        .attr("transform", "translate(-10 5)");
+        .append("circle")
+        .attr("class", "total")
+        .attr("x", svgSizeX / 2)
+        .style("fill", "white")
+        .attr("y", svgSizeY / 2)
+        .attr("transform", "translate(8 10)")
+        .attr("r", 10);
 
-      gText
-        .append("text")
-        .text(booksNo)
-        .attr("class", "book")
-        .attr("class", "cluster-text")
-        .attr("transform", "translate(0 23)");
+      gSymbols.append(() => {
+        var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        g.innerHTML = dBook;
+        return g;
+      });
     }
 
     return L.divIcon({
@@ -201,12 +259,11 @@ export default class MapComponent extends React.Component<Props> {
   }
 
   render() {
-    const icon = L.icon({
+    L.icon({
       iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
       iconSize: [25, 40],
       iconAnchor: [12, 40]
     });
-    console.log(this.props);
 
     return (
       <div className="map" data-testid="map-wrapper">
