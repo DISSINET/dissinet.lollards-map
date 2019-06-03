ba6a93571b2bb8249bc1d6415d7653700f21641b
adammmertel <mertel.adam@gmail.com>
2019-06-03 08:58:09 +0200

    :hammer:REFACTOR: Welcome text content revised

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -40,57 +40,75 @@ export default class WelcomeComponent extends React.Component<Props> {
         <div className="content ">
           <Hero />
           <button
-            className="text-base text-black text-2xl absolute pin-t pin-r"
+            className="text-base text-black text-2xl absolute pin-t pin-r bg-transparent"
             onClick={this.handleClose.bind(this)}
           >
             x
           </button>
           <div className="px-6 py-4">
-            <p className="paragraph">
+            <p>
               This interactive map is based on the{" "}
-              <strong>Lollard trials</strong>. It shows the places of origin of
-              English dissidents under investigation for taking part in the
-              revolts of 1414 or 1431, related to Lollardy, or holding various
-              heretical opinions that are commonly referred to as Lollard, with
-              overlaps between these two criteria. In addition, it shows in
-              which of these places the presence of unauthorised English
-              religious books was uncovered.
+              <span className="text-bold">Lollard trials.</span> It shows the
+              places of origin of English dissidents under investigation for
+              taking part in{" "}
+              <span className="text-bold">the revolts of 1414 or 1431</span>,
+              related to Lollardy, or holding various{" "}
+              <span className="text-bold">heterodox opinions</span> that are
+              commonly referred to as Lollard. In addition, it shows in which of
+              these places the owners of{" "}
+              <span className="text-bold">
+                unauthorised English religious books
+              </span>{" "}
+              were uncovered.{" "}
             </p>
-            <p className="paragraph indent">
-              A total of <strong>260</strong> sites were transferred from the
-              Atlas zur Kirchengeschichte by Jedin et al. (data compiled by M.
-              Lambert based on the trial records analysis research of J. A. F.
-              Thomson and J. Fines). The map shows all sites from Jedin et al.,
-              including those mentioned in the text only, but excluding three
-              sites which could not be localised with enough certainty. The
-              sites inform about the presence of Lollards only, not their
-              numbers or importance.
+
+            <p className=" indent">
+              {" "}
+              A total of <span className="text-bold">260 sites</span> were
+              transferred from the{" "}
+              <span className="text-italic">Atlas zur Kirchengeschichte</span>{" "}
+              by Jedin et al. (data compiled by M. Lambert based on the analysis
+              of trial records by J. A. F. Thomson and J. Fines). The map shows
+              all sites from Jedin et al., including those mentioned only in the
+              accompanying text (by M. Lambert), but excluding three sites which
+              could not be localised with enough certainty. The plotted sites
+              indicate only the presence of suspected Lollards and revolt
+              participants only, not their numbers or importance.{" "}
             </p>
-            <p className="paragraph indent">
-              The period covered ranges from <strong>1414</strong> until{" "}
-              <strong>1522</strong>. After this date, it becomes hard to
-              distinguish between Lollardy and the various forms of Reformation
-              brought from the mainland. The map considers only the cases
-              brought to trial that are covered in extant records and should
-              not, of course, be considered a “complete” map of Lollardy.
+
+            <p className=" indent">
+              {" "}
+              The <span className="text-bold">period covered</span> ranges from{" "}
+              <span className="text-bold">1414</span> until{" "}
+              <span className="text-bold">1522</span>. After this date, it
+              becomes hard to distinguish between Lollardy and the various forms
+              of Protestantism that came over from the continent. The map plots
+              only those locations mentioned in extant trial records and should
+              not, of course, be considered a “complete” map of Lollardy.{" "}
             </p>
-            <p className="paragraph indent">
-              The map can be filtered using these variables: participation in
-              the <strong>Oldcastle Revolt of 1414</strong>, involvement in the
-              smaller <strong>Lollard insurrection of 1431</strong> originating
-              in London, and unauthorised use of{" "}
-              <strong>English religious books</strong>. Unlike Lambert, we do
-              not draw a clear line between Lollard and other participation in
-              the revolts on the map, but this distinction is preserved in the
-              dataset.
+
+            <p className="indent">
+              {" "}
+              The map can be filtered using these variables: participation in{" "}
+              <span className="text-bold">the Oldcastle Revolt of 1414</span>,
+              involvement in the smaller{" "}
+              <span className="text-bold">Lollard insurrection of 1431</span>{" "}
+              originating in London, and unauthorised use of{" "}
+              <span className="text-bold">English religious books</span>. Unlike
+              Lambert, we do not draw a clear line between the presence of
+              Lollards and people related to the revolts for other possible
+              reasons, but this distinction is preserved in the dataset.
             </p>
-            <p className="paragraph indent">
-              The info box under each point on the map displays the place name
-              and lists all the years to which evidence concerning heresy or
-              participation in revolts relates. The list also includes years
-              which do not appear in the sourceJedin et al.’s map and are only
-              mentioned in the accompanying text.
+
+            <p className="indent">
+              The <span className="text-bold">info box</span> under each point
+              on the map displays the place name and lists all the years to
+              which evidence concerning heresy or participation in revolts
+              relates. The list also includes years which do not appear in the
+              source map and are only mentioned in the accompanying text by M.
+              Lambert.
             </p>
+
             <p className="my-2">
               <i className="mx-2 icon icon-book" />
               Data sources:{" "}
