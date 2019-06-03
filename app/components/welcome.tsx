6749638439f47a0cf5962b78e24892b3052f17cf
adammmertel <mertel.adam@gmail.com>
2019-06-03 10:05:40 +0200

    :sparkles:FEAT:  Citation date

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -35,6 +35,13 @@ export default class WelcomeComponent extends React.Component<Props> {
   }
 
   render() {
+    const dateOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    };
+    const now = new Date();
+
     return (
       <div className="welcome" data-testid="welcome-wrapper">
         <div className="content ">
@@ -142,8 +149,9 @@ export default class WelcomeComponent extends React.Component<Props> {
               <b>Recommended citation:</b> Král, J., & Mertel, A. (2019).
               Lollards, 1414–1522: Places of origin of Lollards, unauthorized
               book owners, and participants in revolts related to Lollardy based
-              on their trials. Dissident Networks Project (DISSINET). Retrieved
-              May 25, 2019, from https://dissinet.cz/???.
+              on their trials. Dissident Networks Project (DISSINET). Retrieved{" "}
+              {now.toLocaleDateString("en-US", dateOptions)}, from
+              https://dissinet.cz/lollards-map.
             </p>
           </div>
           <div className="footer w-100 bg-grey p-4">
