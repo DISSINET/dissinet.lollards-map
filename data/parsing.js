243cb67a794112e2be42106829952e0af8fd1634
adammertel <12932677+adammertel@users.noreply.github.com>
2023-10-11 22:58:30 +0200

    Remove turf dependency

--- data/parsing.js
+++ data/parsing.js
@@ -1,5 +1,4 @@
 var GoogleSpreadsheet = require("google-spreadsheet");
-var turf = require("turf");
 var fs = require("fs");
 
 var readSpreadsheet = (key, next) => {
@@ -15,13 +14,13 @@ var readSpreadsheet = (key, next) => {
         next([]);
       }
       next(
-        rows.map(row => {
+        rows.map((row) => {
           delete row["_xml"];
           delete row["_links"];
           delete row["save"];
           delete row["del"];
           const o = {};
-          Object.keys(row).forEach(k => (o[k] = row[k]));
+          Object.keys(row).forEach((k) => (o[k] = row[k]));
           return o;
         })
       );
@@ -30,11 +29,21 @@ var readSpreadsheet = (key, next) => {
 };
 
 const points = [];
-readSpreadsheet("1SUUjjtBT7HBOS9V6USNPk4eR5SFSsSrDByRxrERgg7A", rows => {
+readSpreadsheet("1SUUjjtBT7HBOS9V6USNPk4eR5SFSsSrDByRxrERgg7A", (rows) => {
   rows
-    .filter(r => r.x && r.y)
-    .map(row => {
-      const point = turf.point([parseFloat(row.y), parseFloat(row.x)], row);
+    .filter((r) => r.x && r.y)
+    .map((row) => {
+      // TODO turf was removed
+      // if you want to run this script, check if this is implemented correctly
+      // const point = turf.point([parseFloat(row.y), parseFloat(row.x)], row);
+      const point = {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [parseFloat(row.y), parseFloat(row.x)],
+        },
+        properties: row,
+      };
       points.push(point);
     });
 
